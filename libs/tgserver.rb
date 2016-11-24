b6900dfd009bee3cae14c75e3ad773cb4212b14a
Vladislav Fursov <ghostrussian@gmail.com>
2016-11-25 02:51:41 +0900

    add inline keyboard for inventory. refactoring.

--- libs/tgserver.rb
+++ libs/tgserver.rb
@@ -38,47 +38,59 @@ class TelegramServer
 
   def server(token, game)
     Telegram::Bot::Client.run(token) do |bot|
-
       @bot = bot
 
       bot.listen do |message|
-        if message.text.empty?
-          next
-        end
-
-        sender = Registration.GetLoginOfType(message.from.id, TYPE)
-
-        unless game.players.key?(sender)
-          unless @regMessage[sender]
-            @regMessage[sender] = true
-            bot.api.send_message(chat_id: message.from.id, text: "Необходимо пройти регистрацию (всего 7 вопросов). Просклоняйте имя вашего персонажа:")
-          end
-
-          ret = @reg.RegPlayer(message.from.id, message.text)
-          if ret == @reg.questions_messages[6] # sex
-            bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @sexKey)
-            next
-
-          elsif ret == @reg.questions_messages[7] # confirm
-            bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @confirm)
-            next
-
-          elsif ret == @reg.registred_message
-            @jb.parse_command(sender, 'start')
-            bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @dirKey)
-            next
-          end
-          bot.api.send_message(chat_id: message.from.id, text: ret)
-          next
-        end
-
-        begin
-          @jb.parse_command(sender, message.text)
-        rescue => detail
-          log "\nОшибка #{TYPE} при отправке команды: #{message.text} - игроком: #{sender}, боту: #{$!.to_s}\n"+detail.backtrace.join("\n")
+        case message
+          when Telegram::Bot::Types::CallbackQuery
+            callbackHandler(bot, message)
+          when Telegram::Bot::Types::Message
+            messageHandler(bot, message)
         end
       end
     end
+  end
+
+  def messageHandler(bot, message)
+    if message.text.empty?
+      return
+    end
+
+    sender = Registration.GetLoginOfType(message.from.id, TYPE)
+
+    unless game.players.key?(sender)
+      unless @regMessage[sender]
+        @regMessage[sender] = true
+        bot.api.send_message(chat_id: message.from.id, text: "Необходимо пройти регистрацию (всего 7 вопросов). Просклоняйте имя вашего персонажа:")
+      end
+
+      ret = @reg.RegPlayer(message.from.id, message.text)
+      if ret == @reg.questions_messages[6] # sex
+        bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @sexKey)
+        return
+
+      elsif ret == @reg.questions_messages[7] # confirm
+        bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @confirm)
+        return
+
+      elsif ret == @reg.registred_message
+        @jb.parse_command(sender, 'start')
+        bot.api.send_message(chat_id: message.from.id, text: ret, reply_markup: @dirKey)
+        return
+      end
+      bot.api.send_message(chat_id: message.from.id, text: ret)
+      return
+    end
+
+    begin
+      @jb.parse_command(sender, message.text)
+    rescue => detail
+      log "\nОшибка #{TYPE} при отправке команды: #{message.text} - игроком: #{sender}, боту: #{$!.to_s}\n"+detail.backtrace.join("\n")
+    end
+  end
+
+  def callbackHandler(bot, message)
+    inventoryCallback(bot, message)
   end
 
   def deliver(sender, txt)
@@ -98,13 +110,12 @@ class TelegramServer
 
       # генерация клавиатуры для помощи
       if txt =~ /^Категории команд:\n/i
-        help_kb = []
-        help_split = txt.split(/\n/).drop(2).reverse.drop(2).reverse
-        help_split.each do |name|
-          help_kb.push(["? #{name}"])
-        end
+        keyboard = generateHelpKeyboard(txt)
+      end
 
-        keyboard = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: help_kb, one_time_keyboard: true)
+      # генерация инлайн клавиатуры для инвентаря
+      if txt =~ /^У тебя есть:\n/i
+        keyboard = generateInventoryKeyboard(txt)
       end
 
       # стартовая клава при выходе и автовыходе
@@ -127,5 +138,78 @@ class TelegramServer
 
   def log(mes)
     $stderr.puts "\n[#{Time.now.to_s}]: "+mes
+  end
+
+  private
+
+  # help
+  def generateHelpKeyboard(message)
+    help_kb = []
+    help_split = message.split(/\n/).drop(2).reverse.drop(2).reverse
+    help_split.each do |name|
+      help_kb.push(["? #{name}"])
+    end
+
+    return Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: help_kb, one_time_keyboard: true)
+  end
+
+  # inventory
+  INVENTORY_TYPES = [
+      ["Осмотреть", "look"],
+      ["Использовать", "use"],
+      ["Выложить", "miscarry"]
+  ]
+
+  def generateInventoryKeyboard(message)
+    kb = []
+
+    INVENTORY_TYPES.each { |elem|
+      kb.push(Telegram::Bot::Types::InlineKeyboardButton.new(text: elem[0], callback_data: "Inventory-#{elem[1]}"))
+    }
+
+    return Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+  end
+
+  def inventoryCallback(bot, message)
+    invData = /^Inventory-(?<type>[a-z]*)(-(?<index>[0-9]{1,3}))?$/i.match(message.data).to_a
+
+    if !invData.nil?
+      kb = []
+      addText = ''
+
+      if invData[2].nil?
+        line_kb = []
+        inv_split = message.message.text.split(/\n/).drop(1)
+
+        for i in 1..inv_split.length
+          line_kb.push(Telegram::Bot::Types::InlineKeyboardButton.new(text: "#{i}", callback_data: "Inventory-#{invData[1]}-#{i}"))
+
+          if i % 5 == 4
+            kb.push(line_kb.clone)
+            line_kb = []
+          end
+        end
+        kb.push(line_kb.clone)
+
+        INVENTORY_TYPES.each { |elem|
+          if elem[1] == invData[1]
+            addText = "\n\n>> #{elem[0]}:"
+          end
+        }
+      else
+        INVENTORY_TYPES.each { |elem|
+          if elem[1] == invData[1]
+            addText = " #{invData[2]}"
+            @jb.parse_command(Registration.GetLoginOfType(message.from.id, TYPE), "#{elem[0]} #{invData[2]}")
+          end
+        }
+      end
+
+      begin
+        bot.api.editMessageText(chat_id: message.from.id, message_id: message.message.message_id, text: message.message.text + addText, reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb))
+      rescue
+        # ignore telegram error
+      end
+    end
   end
 end
